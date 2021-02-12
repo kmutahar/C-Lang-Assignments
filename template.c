fe93074d0494049268ddf3077b96540e419d18c4
Khaldoon Mutahar <git@mutahar.me>
2021-02-12 19:32:55 +0300

    add C lang blank template

---
+++ template.c
@@ -0,0 +1,16 @@
+/*
+  Khaldoon Mutahar (61930208)
+  CSCI250 - Introduction to Programming
+  Mr. Omar Al-Fakih
+
+  {{ What the program does }}
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
